5e8192967b7c2ca7dd1824b0811cf9e52b4182dc
DarioGalleni <dariogalleni@yahoo.it>
2024-02-13 16:03:39 +0100

    a metà via

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -37,4 +37,16 @@ class Article extends Model
     {
         return $this->hasMany(Review::class);
     }
+
+    public function setAccepted($value)
+    {
+        $this->is_accepted = $value;
+        $this->save();
+        return true;
+    }
+
+    public static function toBeRevisionedCount()
+    {
+        return Article::where('is_accepted', null)->count();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\RevisorController;
 use App\Http\Controllers\CategoryController;
 
 Route::get('/',[PublicController::class, 'home'])->name('home');
@@ -18,4 +19,13 @@ Route::get('/index',[ArticleController::class,'index'])->name('index');
 // Rotte recensioni
 Route::get('/create/review/{article}',[ReviewController::class,'create'])->name('create_review');
 Route::post('/store/review/{article}',[ReviewController::class,'store'])->name('store_review');
+
+
+//revisore
+Route::get('/revisor/home', [RevisorController::class, 'index'])->name('revisor.index');
+Route::patch('/accetta/annuncio/{article}', [RevisorController::class, 'acceptArticle'])->name('revisor.accept_article');
+Route::patch('/rifiuta/annuncio/{article}', [RevisorController::class, 'rejectArticle'])->name('revisor.reject_article');
+
+
+
 
