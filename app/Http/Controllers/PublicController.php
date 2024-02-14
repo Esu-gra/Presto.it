871cbd2292018a4a0699552d67016aff7ce26b7e
DarioGalleni <dariogalleni@yahoo.it>
2024-02-14 09:36:12 +0100

    Pronti partenza via

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -10,11 +10,8 @@ class PublicController extends Controller
 
     public function home()
 {
-   
     $articles = Article::where('is_accepted', true)->take(6)->get()->sortByDesc('created_at');
     return view('welcome', compact('articles'));
-
-    
 }
 
 
